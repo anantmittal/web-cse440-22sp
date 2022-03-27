5b2ba0971f43cf2f820faf4d9c2efc9a0f2a7072
James Fogarty <jayfo@users.noreply.github.com>
2022-03-27 07:15:06 -0700

    Add Assignment 3

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -23,6 +23,7 @@ import AssignmentsOverviewWrapper from "src/content/assignments/OverviewWrapper.
 import Assignment0 from "src/content/assignments/assignment0/Assignment0.mdx"
 import Assignment1 from "src/content/assignments/assignment1/Assignment1.mdx"
 import Assignment2 from "src/content/assignments/assignment2/Assignment2.mdx"
+import Assignment3 from "src/content/assignments/assignment3/Assignment3.mdx"
 
 export const App: React.FunctionComponent = () => {
     return (
@@ -49,6 +50,7 @@ export const App: React.FunctionComponent = () => {
                             <Route path="assignment0" element={<Assignment0/>} />
                             <Route path="assignment1" element={<Assignment1/>} />
                             <Route path="assignment2" element={<Assignment2/>} />
+                            <Route path="assignment3" element={<Assignment3/>} />
                         </Route>
                         <Route path="/calendar" element={<Calendar/>} />
                     </Routes>
