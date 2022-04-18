bdfdddfd11eb7a30f86492f0e7205a0ff4090ef3
James Fogarty <jayfo@users.noreply.github.com>
2022-04-18 08:39:11 -0700

    Improved Documentation

--- src/components/AssignmentSamples.tsx
+++ src/components/AssignmentSamples.tsx
@@ -44,19 +44,29 @@ export const AssignmentSamples: FunctionComponent<AssignmentSamplesProps> = (pro
             </p>
             {
                 store.courseInformation.projectSamples.map(
-                    sampleCurrent =>  (renderMilestone in sampleCurrent.samples) && (
-                        <React.Fragment key={sampleCurrent.name + props.milestone}>
-                            <p>
-                                <GeneratedLink href={sampleCurrent.samples[renderMilestone]}>
-                                    {'Sample ' + props.milestone}
-                                </GeneratedLink>
-                                {' from '}
-                                {
-                                    (props.milestone == '1c') && 'the proposal that became '
-                                }
-                                <GeneratedLink href={sampleCurrent.link}>{sampleCurrent.name}</GeneratedLink>
-                            </p>
-                        </React.Fragment>
+                    sampleCurrent => (
+                        // Ensure we have a sample for this project
+                        (renderMilestone in sampleCurrent.samples) &&
+                        // Render the sample
+                        (
+                            <React.Fragment
+                                // Generate a unique key using props.milestone.
+                                // Do not use renderMilestone because they may not be equal,
+                                // which could create a situation with non-unique keys.
+                                key={props.milestone + sampleCurrent.name}
+                            >
+                                <p>
+                                    <GeneratedLink href={sampleCurrent.samples[renderMilestone]}>
+                                        {'Sample ' + props.milestone}
+                                    </GeneratedLink>
+                                    {' from '}
+                                    {
+                                        (props.milestone == '1c') && 'the proposal that became '
+                                    }
+                                    <GeneratedLink href={sampleCurrent.link}>{sampleCurrent.name}</GeneratedLink>
+                                </p>
+                            </React.Fragment>
+                        )
                     )
                 )
             }
